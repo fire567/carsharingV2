9f3712e7238c916e0e6620815afb30d4fa1e9fc0
fire567 <dante.nero.95@gmail.com>
2022-03-15 21:10:53 +0300

    gh-pages-test

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from "redux";
 import { Provider }  from "react-redux";
+import { HashRouter } from "react-router-dom";
 import reducers from "./reducers"
 import App from './App';
 
 const store= createStore(reducers);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <HashRouter basename={process.env.PUBLIC_URL}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>,
   document.getElementById('root')
 );
