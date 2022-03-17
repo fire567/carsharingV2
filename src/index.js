8760f1bdc716b8c1f48c86c01649ff6ddbafd596
fire567 <dante.nero.95@gmail.com>
2022-03-17 18:31:12 +0300

    v1f1_fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from "redux";
 import { Provider }  from "react-redux";
-import reducers from "./reducers"
+import reducers from "./Redux/reducers"
 import App from './App';
 
 const store= createStore(reducers);
