6f6b94d845ea7a6c940e54221fc9a976cecb86bf
fire567 <dante.nero.95@gmail.com>
2022-03-21 11:41:18 +0300

    f1/v1 conts fix

--- src/Components/Carousel/Slides/Slides.js
+++ src/Components/Carousel/Slides/Slides.js
@@ -1,43 +1,9 @@
 import React from "react";
-import car1 from "../../../assets/car1.svg";
-import car2 from "../../../assets/car2.svg";
-import car3 from "../../../assets/car3.svg";
-import car4 from "../../../assets/car4.svg";
+import { carouselItems } from "../../../consts";
 import classes from "./Slides.module.css";
 
 
 const Slides = ({position}) => {
-
-    const carouselItems = [
-        {
-            id: 0, 
-            header: "Бесплатная парковка", 
-            text:"Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.",
-            color: "linear-gradient(90deg, #13493F 0%, #0C7B1B 100%)",
-            img: car1,
-        },
-        {
-            id: 1, 
-            header: "Страховка", 
-            text:"Полная страховка страховка автомобиля",
-            color: "linear-gradient(90deg, #132949 0%, #0C7B67 100%)",
-            img: car2,
-        },
-        { 
-            id:2, 
-            img:car3,
-            header:"Бензин", 
-            text:"Полный бак на любой заправке города за наш счёт",
-            color:"linear-gradient(90deg, #493013 0%, #7B0C3B 100%)"
-        },
-        { 
-            id:3, 
-            img:car4,
-            header:"Обслуживание", 
-            text:"Автомобиль проходит еженедельное ТО",
-            color:"linear-gradient(90deg, #281349 0%, #720C7B 100%)"
-         }
-    ]
 
     return(
         carouselItems.map((item) => (
