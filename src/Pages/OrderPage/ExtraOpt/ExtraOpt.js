63364ef9629d2019773a5f5e2500c937b8f1a7fa
fire567 <dante.nero.95@gmail.com>
2022-03-29 13:08:37 +0300

    datepicker fix

--- src/Pages/OrderPage/ExtraOpt/ExtraOpt.js
+++ src/Pages/OrderPage/ExtraOpt/ExtraOpt.js
@@ -11,7 +11,9 @@ const ExtraOpt = () => {
   const currentCar = useSelector((state) => state.currentCar);
   const color = useSelector((state) => state.color);
   const rates = useSelector((state) => state.rates);
-  const [sinceDate, setSinceDate] = useState(null);
+  const [sinceDate, setSinceDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const [isDisabled, setDisabled] = useState(true);
 
   const extraOptions = [
     { id: 0, value: 'Полный бак, 500р', name: 'Полный бак' },
@@ -19,12 +21,33 @@ const ExtraOpt = () => {
     { id: 2, value: 'Правый руль, 1600р', name: 'Правый руль' },
   ];
 
+  useEffect(() => {
+    if (sinceDate) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [sinceDate]);
+
   const colorHandler = (currentColor) => {
     dispatch(setColor(currentColor));
   };
 
   const sinceDateHandler = (date) => {
     setSinceDate(date);
+  };
+
+  const endDateHandler = (date) => {
+    setEndDate(date);
+  };
+
+  const deleteEndDate = () => {
+    setEndDate(null);
+  };
+
+  const deleteSinceDate = () => {
+    setSinceDate(null);
+    setEndDate(null);
   };
 
   useEffect(() => {
@@ -37,8 +60,8 @@ const ExtraOpt = () => {
       <div className={classes.opt_form}>
         <div className={classes.header}>Цвет</div>
           <div className={classes.colors_form}>
-          {currentCar.colors.map((item) => (
-            <div className={classes.opt_colors_form} key={item}>
+          {currentCar.colors.map((item, index) => (
+            <div className={classes.opt_colors_form} key={index}>
               <div className={color === item ? classes.circle_active : classes.circle} onClick={() => colorHandler(item)}></div>
               <div className={classes.opt_value}>{item}</div>
             </div>
@@ -52,19 +75,28 @@ const ExtraOpt = () => {
         <div className={classes.inputs_form}>
           <div className={classes.date_form}>
             <label className={classes.label}>С </label>
-            <span className={classes.close}/>
+            <span className={classes.close} onClick={deleteSinceDate}/>
             <DatePicker
               className={classes.date}
               selected={sinceDate}
-              onChange={(date) => sinceDateHandler(date)}
+              filterTime={false}
+              minDate={new Date()}
+              onChange={sinceDateHandler}
+              showTimeInput
+              dateFormat="dd/MM/yyyy HH:mm"
               placeholderText={'Введите дату и время'}
             />
           </div>
           <div className={classes.date_form}>
             <label className={classes.label}>По </label>
-            <span className={classes.close}/>
+            <span className={classes.close} onClick={deleteEndDate}/>
             <DatePicker
               className={classes.date}
+              selected={endDate}
+              onChange={(date) => endDateHandler(date)}
+              showTimeInput
+              disabled={isDisabled}
+              dateFormat="dd/MM/yyyy hh:mm"
               placeholderText={'Введите дату и время'}
             />
           </div>
